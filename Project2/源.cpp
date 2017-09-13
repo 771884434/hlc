4f7b15fad6a5f979cd9934523447edb1591ce77b
Hou Lichao <771884434@qq.com>
2017-09-13 16:43:07 +0800

    try

--- Project2/源.cpp
+++ Project2/源.cpp
@@ -1,8 +1,12 @@
 #include <stdio.h>
 #include <WinSock2.h>
+#include <iostream>
+using namespace std;
 
 int main(int argc, char* argv[]){
 	WORD sockVersion = MAKEWORD(2, 2);
 	WSADATA wsaData;
-	if (WSAStartup(sockVersion,&wsaData)!=0)
+	if (WSAStartup(sockVersion, &wsaData) != 0);
+	cout << "hou" << endl;
+	asdhjdjkhjkjhlkcznxlkjfklqa;
 }
